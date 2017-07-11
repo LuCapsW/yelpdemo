9df56a5f03e35339773b73ddbeea249fedd88e91
Lucas <lh.wong89@gmail.com>
2017-07-11 21:56:08 +0200

    unsaved carrierwave file

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,10 +2,10 @@ CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws'                        # required
   config.fog_credentials = {
     provider:              'AWS',                        # required
-    aws_access_key_id:     'AKIAJYAX7X4L6L7CI2MQ',                        # required
-    aws_secret_access_key: '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',                        # required
+    aws_access_key_id:      ENV["aws_access_key_id"],                        # required
+    aws_secret_access_key:  ENV["aws_secret_access_key" ,                        # required
 	region:                'eu-west-2',                  # optional, defaults to 'us-east-1'  
   }
 
-  config.fog_directory  = 'yelpdemolucasw'                          # required
+  config.fog_directory  = ENV["fog_directory"]                         # required
 end
